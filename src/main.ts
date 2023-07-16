a01e833c6e3608a3e457605f3e0bf417cb5a896f
Md Nasim us Sabah Shishir <tech17196@gmail.com>
2023-07-16 15:03:50 +0600

    CORS added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    // Other CORS options can be set here (e.g., allowedHeaders, exposedHeaders, etc.)
+  });
+
   await app.listen(8000);
 }
 bootstrap();
