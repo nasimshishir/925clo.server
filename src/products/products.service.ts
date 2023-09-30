ec0fd2f34ffdacec15f55e393bae2cefb569236c
Md Nasim us Sabah Shishir <tech17196@gmail.com>
2023-10-01 02:00:26 +0600

    product add function destructure-8

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -21,39 +21,69 @@ export class ProductsService {
   ) { }
 
   async destructureProducts(productDetails: CreateProductParams[]) {
+    await Promise.all(
+      productDetails.map(async (product) => {
 
-    const allProducts = productDetails.map(async (product) => {
-      const singleProduct = {
-        product_id: product.product_id,
-        product_title: product.product_title,
-        description: product.description,
-        image: product.image,
-        price: product.price,
-        product_category: product.product_category,
-        type: product.type,
-        gender: product.gender,
-        product_url: product.product_url,
-        occasion: product.occasion,
-        season: product.season,
-        primaryColor: null,
-        secondaryColor: null,
-        sizes: [],
-        brand: null,
-      }
-      const newProduct = this.productRepository.create(singleProduct);
-
-      product.sizes.map(async (size) => {
-        if (size.stock) {
-          const sizeDB = await this.sizesRepository.findOne({ where: { size: size.size } })
-          // return sizeDB.product.push(newProduct)
-          return newProduct.sizes.push(sizeDB)
+        const singleProduct = {
+          product_id: product.product_id,
+          product_title: product.product_title,
+          description: product.description,
+          image: product.image,
+          price: product.price,
+          product_category: product.product_category,
+          type: product.type,
+          gender: product.gender,
+          product_url: product.product_url,
+          occasion: product.occasion,
+          season: product.season,
+          primaryColor: null,
+          secondaryColor: null,
+          sizes: [],
+          brand: null,
         }
+        const newProduct = this.productRepository.create(singleProduct);
+
+        for (let i = 0; i < product.sizes.length; i++) {
+          let size = product.sizes[i];
+          if (size.stock) {
+            const sizeDB = await this.sizesRepository.findOne({ where: { size: size.size } })
+
+            newProduct.sizes.push(sizeDB)
+          }
+        }
+        const primaryColor = async (product: CreateProductParams): Promise<Colors> => {
+          if (product.color.primary) {
+            const colorDB = await this.colorsRepository.findOne({ where: { color: product.color.primary } })
+            if (!colorDB) {
+              const primaryColor = this.colorsRepository.create({ color: product.color.primary })
+              return await this.colorsRepository.save(primaryColor);
+            } else {
+              return colorDB;
+            }
+          } else {
+            return null
+          }
+        }
+        const secondaryColor = async (product: CreateProductParams): Promise<Colors> => {
+          if (product.color.secondary) {
+            const colorDB = await this.colorsRepository.findOne({ where: { color: product.color.secondary } })
+            if (!colorDB) {
+              const secondaryColor = this.colorsRepository.create({ color: product.color.secondary })
+              return await this.colorsRepository.save(secondaryColor);
+            } else {
+              return colorDB;
+            }
+          } else {
+            return null
+          }
+        }
+        newProduct.primaryColor = await primaryColor(product)
+        newProduct.secondaryColor = await secondaryColor(product)
+
+
+        await this.productRepository.save(newProduct);
       })
-
-      return await this.productRepository.save(newProduct);
-      // return singleProduct
-
-    })
+    );
   }
 
   async createProduct() {
@@ -61,7 +91,14 @@ export class ProductsService {
   }
 
   async findAll(gender, type, size,): Promise<Products[]> {
-    return await this.productRepository.find({ where: { gender: `${gender}` }, });
+    return await this.productRepository.find({
+      where: {
+        gender: `${gender}`
+      },
+      relations: {
+        primaryColor: true
+      }
+    });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
