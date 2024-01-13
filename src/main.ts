c89c2860ec33f770435b25bc387bed90946afd1f
nasimshishir <tech17196@gmail.com>
2024-01-13 10:57:27 +0600

    Deploy trial-2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1')
   app.enableCors({
     origin: true,
     // Other CORS options can be set here (e.g., allowedHeaders, exposedHeaders, etc.)
