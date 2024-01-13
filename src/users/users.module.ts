e47ce676b3374316460b21f2da6fd5bd879390ce
nasimshishir <tech17196@gmail.com>
2024-01-13 16:27:40 +0600

    Deploy trail-4

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersController } from './users.controller';
 import { Users } from './entities/user.entity';
 import { EmailToken } from './entities/email-verification-token.entity';
 import { UserInteractions } from './entities/inreractions.entity';
+import { Wishlist } from './entities/wishlist.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users, EmailToken, UserInteractions])],
+  imports: [TypeOrmModule.forFeature([Users, EmailToken, UserInteractions, Wishlist])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
